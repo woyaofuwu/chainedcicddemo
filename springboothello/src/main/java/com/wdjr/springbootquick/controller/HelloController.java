15644f91cfbdf58e1592a6cce679e130e2d7e303
jaysonzhao <jaysonzhao@vip.qq.com>
2020-05-17 21:22:54 +0800

    fix

--- springboothello/src/main/java/com/wdjr/springbootquick/controller/HelloController.java
+++ springboothello/src/main/java/com/wdjr/springbootquick/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import pl.sf.test;
+import pl.sf.test.Runner;
 
 //这个类的所有方法返回的数据直接返回给浏览器（如果是对象转为json数据）
 /*@ResponseBody
